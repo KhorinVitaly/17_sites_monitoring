4b6d1a097e200cde4c3feaac7305a98760e7dfe3
KhorinVitaly <khorin.v@gmail.com>
2017-09-13 09:57:38 +0600

    first commit
    realize task

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+With this python script you can check website status and domain expiration date.
+
+# How to Install
+
+Python 3 should be already installed. 
+
+# How to Use
+
+Fill the text file with urls, for example urls.txt 
+
+```
+https://devman.org
+https://bitbucket.org
+```
+
+Launch check_sites_health.py in terminal. Transfer file path to text file as argument.
+```
+python3 check_sites_health.py -p urls.txt
+
+https://devman.org
+Server responded with HTTP 200 status
+Domain is available for one month at least
+https://bitbucket.org
+Server responded with HTTP 200 status
+Domain is available for one month at least
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,65 @@
+import whois
+import requests
+from datetime import datetime
+import argparse
+from os.path import exists
+
+
 def load_urls4check(path):
-    pass
+    if not exists(path):
+        return None
+    with open(path, "r") as urls_file:
+        return urls_file.read().split('\n')
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        OK_status_code = 200
+        responce = requests.get(url)
+        if responce.status_code == OK_status_code:
+            return True
+        else:
+            return False
+    except:
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain = whois.whois(domain_name)
+    if type(domain.expiration_date) == list:
+        return domain.expiration_date[0].date()
+    else:
+        return domain.expiration_date.date()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", action="store",
+                        help="file with url for check")
+    return parser.parse_args()
+
+
+def print_urls_information(urls):
+    for url in urls:
+        print(url)
+        if is_server_respond_with_200(url):
+            print('Server responded with HTTP 200 status')
+        else:
+            print('Server did NOT respond with HTTP 200 status')
+
+        current_date = datetime.today()
+        next_month = current_date.replace(month=current_date.month+1).date()
+        expiration_date = get_domain_expiration_date(url)
+        if next_month < expiration_date:
+            print('Domain is available for one month at least')
+        else:
+            print('Domain is available until ' + expiration_date)
 
 if __name__ == '__main__':
-    pass
+    args = parse_arguments()
+    if not args.path:
+        print('File with url for check is not specified!')
+        exit()
+    urls = load_urls4check(args.path)
+    print_urls_information(urls)
+
